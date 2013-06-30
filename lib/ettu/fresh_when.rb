21fbf7410fd065829bddb0935ad584f4e87c8349
Justin Ridgewell <justin@ridgewell.name>
2013-06-30 13:28:29 -0400

    Use new Ettu dependency injection in FreshWhen

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -6,13 +6,10 @@ class Ettu
       alias_method :old_fresh_when, :fresh_when
 
       def fresh_when(record_or_options, additional_options = {})
-        ettu = Ettu.new(record_or_options, additional_options)
+        ettu = Ettu.new(self, record_or_options, additional_options)
 
         etags = [*ettu.response_etag]
-        if view = ettu.options.fetch(:view, "#{controller_name}/#{action_name}")
-          # TODO: This is ugly. How do we clean it up?
-          etags << ettu.view_etag(view, request.format.try(:to_sym), lookup_context)
-        end
+        etags << ettu.view_etag
         if request.format.try(:html?)
           etags << ettu.js_etag
           etags << ettu.css_etag
