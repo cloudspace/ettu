b080e515a1a2dc1ec0da744d4fde13bfe4476b8f
Justin Ridgewell <justin@ridgewell.name>
2013-06-30 19:24:23 -0400

    Use dependency injection in FreshWhen

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -5,14 +5,19 @@ class Ettu
     included do
       alias_method :old_fresh_when, :fresh_when
 
-      def fresh_when(record_or_options, additional_options = {})
-        ettu = Ettu.new(record_or_options, additional_options, self)
+      def fresh_when(record_or_options, additional_options)
+        ettu = ettu_instance(record_or_options, additional_options, self)
 
         ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
 
         old_fresh_when nil, ettu.options.merge(ettu_params)
       end
+    end
 
+    private
+
+    def ettu_instance(record_or_options, additional_options, controller)
+      Ettu.new record_or_options, additional_options, controller
     end
   end
 end
