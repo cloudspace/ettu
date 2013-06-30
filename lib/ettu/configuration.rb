879e9e210699286f4f1ab98cb31b11133fb4434d
Justin Ridgewell <justin@ridgewell.name>
2013-06-30 19:22:17 -0400

    Make sure Configuration.view is reset

--- lib/ettu/configuration.rb
+++ lib/ettu/configuration.rb
@@ -20,6 +20,7 @@ class Ettu
       # This'll allow #fetch to return the real default
       # at runtime.
       # self.view = "#{controller_name}/#{action_name}"
+      delete :view if key? :view
 
       if defined? ActionView::Digestor
         self.template_digestor = ActionView::Digestor
