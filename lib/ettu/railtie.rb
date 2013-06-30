900e11031f91187a9a183b2700ccec509522d4d5
Justin Ridgewell <justin@ridgewell.name>
2013-06-30 02:19:34 -0400

    Allow Ettu to be configured
    Allows Ettu to be disabled using the standard rails config optoins, and
    adds in a flag to workaround
    [#10791](https://github.com/rails/rails/pull/10791).

--- lib/ettu/railtie.rb
+++ lib/ettu/railtie.rb
@@ -1,10 +1,26 @@
 module Ettu
   class Railtie < Rails::Railtie
-    initializer 'install' do
-      require 'ettu'
+    config.ettu = ActiveSupport::OrderedOptions.new
 
-      ActiveSupport.on_load :action_controller do
-        ActionController::Base.send :include, FreshWhen
+    initializer 'load' do |app|
+
+      unless app.config.ettu.disabled
+        require 'ettu'
+        ActiveSupport.on_load :action_controller do
+          ActionController::Base.send :include, FreshWhen
+        end
+
+        if app.config.ettu.development_hack
+          class BlackHole < Hash
+            def []=(k, v); end
+          end
+          module ::ActionView
+            class Digestor
+              @@cache = BlackHole.new
+            end
+          end
+        end
+
       end
     end
   end
