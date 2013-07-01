89974a449eb3c356f99fc51647baf5198236ec24
Justin Ridgewell <justin@ridgewell.name>
2013-06-30 20:39:40 -0400

    Allow additional_options to be optional

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -5,7 +5,7 @@ class Ettu
     included do
       alias_method :old_fresh_when, :fresh_when
 
-      def fresh_when(record_or_options, additional_options)
+      def fresh_when(record_or_options, additional_options = {})
         ettu = ettu_instance(record_or_options, additional_options, self)
 
         ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
