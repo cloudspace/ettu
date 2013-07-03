5b248d05f18fb754b3321dcb944cdcb9821c5153
Justin Ridgewell <justin@ridgewell.name>
2013-07-02 20:55:22 -0400

    Use alias_method_chain to override fresh_when

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -3,18 +3,18 @@ class Ettu
     extend ActiveSupport::Concern
 
     included do
-      alias_method :old_fresh_when, :fresh_when
-
-      def fresh_when(record_or_options, additional_options = {})
-        ettu = ettu_instance(record_or_options, additional_options, self)
-
-        ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
-
-        old_fresh_when nil, ettu.options.merge(ettu_params)
-      end
+      alias_method_chain :fresh_when, :ettu
     end
 
     private
+
+    def fresh_when_with_ettu(record_or_options, additional_options = {})
+      ettu = ettu_instance(record_or_options, additional_options, self)
+
+      ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
+
+      fresh_when_without_ettu nil, ettu.options.merge(ettu_params)
+    end
 
     def ettu_instance(record_or_options, additional_options, controller)
       Ettu.new record_or_options, additional_options, controller
