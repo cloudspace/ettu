451575ed7340011a5313bce77f1a6f08f4178432
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-18 09:51:45 -0400

    Late load Assets

--- lib/ettu/configuration.rb
+++ lib/ettu/configuration.rb
@@ -13,7 +13,7 @@ class Ettu
     private
 
     def set_defaults
-      self.assets = LateLoadAssets.new
+      self.assets = LateLoadAssets.new(self, :assets)
 
       # Don't actually set view by default.
       # This'll allow #fetch to return the real default
@@ -21,40 +21,25 @@ class Ettu
       # self.view = "#{controller_name}/#{action_name}"
       delete :view if key? :view
 
-      self.template_digestor = LateLoadTemplateDigestor.new(self)
+      self.template_digestor = ActionView::Digestor
     end
 
     class LateLoadAssets
-      def initialize
-        @array = []
-      end
-
-      def to_a
-        ::ActionView::Base.assets_manifest.assets.keys + [*@array]
+      def initialize(config, name)
+        @config = config
+        @name = name
       end
 
       def method_missing(method, *args, &block)
-        @array.send method, *args, &block
-      end
-    end
-
-    class LateLoadTemplateDigestor
-      def initialize(config)
-        @config = config
-      end
-
-      def digest(*args)
-        digestor = attempt_late_template_digestor_set
-        digestor.digest(*args)
+        assets = Array.new(defaults)
+        @config[@name] = array
+        assets.send method, *args, &block
       end
 
       private
 
-      def attempt_late_template_digestor_set
-        # Attempt to use ActionView::Digestor
-        if defined? ActionView::Digestor
-          @config.template_digestor = ActionView::Digestor
-        end
+      def defaults
+        ::ActionView::Base.assets_manifest.assets.keys
       end
     end
   end
