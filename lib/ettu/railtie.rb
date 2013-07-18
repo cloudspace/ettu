f350c9b6273e97c7163892df8f93a129e7c83ca9
Justin Ridgewell <jridgewell@cloudspace.com>
2013-07-18 12:56:21 -0400

    Stop etags fresh_when development_hack is true

--- lib/ettu/railtie.rb
+++ lib/ettu/railtie.rb
@@ -1,24 +1,37 @@
 class Ettu
   class Railtie < Rails::Railtie
     config.ettu = ActiveSupport::OrderedOptions.new
-
     initializer 'load' do |app|
 
       unless app.config.ettu.disabled
         require 'ettu'
-        ActiveSupport.on_load :action_controller do
-          ActionController::Base.send :include, FreshWhen
-        end
-
         if app.config.ettu.development_hack
+
           class BlackHole < Hash
             def []=(k, v); end
           end
+          module EtagBuster
+            extend ActiveSupport::Concern
+            included do
+              def fresh_when(*args); end
+            end
+          end
+
           module ::ActionView
             class Digestor
               @@cache = BlackHole.new
             end
           end
+          ActiveSupport.on_load :action_controller do
+            ActionController::Base.send :include, EtagBuster
+          end
+
+        else
+
+          ActiveSupport.on_load :action_controller do
+            ActionController::Base.send :include, FreshWhen
+          end
+
         end
       end
 
