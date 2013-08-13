f82d879858a979723140e56d4503cf4c2c9ff73c
Justin Ridgewell <jridgewell@cloudspace.com>
2013-08-13 14:39:30 -0400

    Let `fresh_when` use `super`

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -2,19 +2,15 @@ class Ettu
   module FreshWhen
     extend ActiveSupport::Concern
 
-    included do
-      alias_method_chain :fresh_when, :ettu
-    end
-
-    private
-
-    def fresh_when_with_ettu(record_or_options, additional_options = {})
+    def fresh_when(record_or_options, additional_options = {})
       ettu = ettu_instance(record_or_options, additional_options, self)
 
       ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
 
-      fresh_when_without_ettu nil, ettu.options.merge(ettu_params)
+      super nil, ettu.options.merge(ettu_params)
     end
+
+    private
 
     def ettu_instance(record_or_options, additional_options, controller)
       Ettu.new record_or_options, additional_options, controller
