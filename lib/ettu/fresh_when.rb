6628d6b37cebce46add5d88ed01b3853f98c0e44
Justin Ridgewell <jridgewell@cloudspace.com>
2013-08-13 15:11:14 -0400

    Fix bad argument anme

--- lib/ettu/fresh_when.rb
+++ lib/ettu/fresh_when.rb
@@ -3,7 +3,7 @@ class Ettu
     extend ActiveSupport::Concern
 
     def fresh_when(record_or_options, additional_options = {})
-      ettu = Ettu.new record_or_options, additional_options, controller
+      ettu = Ettu.new record_or_options, additional_options, self
 
       ettu_params = {etag: ettu.etags, last_modified: ettu.last_modified}
 
