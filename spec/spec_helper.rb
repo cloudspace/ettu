ae87e38a58bfab5c5516f4e62ec8150811276385
Justin Ridgewell <justin@ridgewell.name>
2014-03-12 20:27:14 -0400

    Require support files relative to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../dummy/config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'date'
@@ -8,7 +8,7 @@ require 'securerandom'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[File.join(File.expand_path('..', __FILE__), 'support/**/*.rb')].each { |f| require f }
 
 require 'ettu'
 
@@ -25,5 +25,5 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = 'random'
 end
